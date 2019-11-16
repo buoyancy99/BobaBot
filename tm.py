554566df742a14e75f6c33e9ab32d08286712718
victorychain <victorcheng21@berkeley.edu>
2019-11-15 18:01:18 -0800

    multi tm

--- tm.py
+++ tm.py
@@ -93,6 +93,88 @@ def tm(img, template, prev_loc=None):
     cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
     return maxLoc
 
+
+def tm_multi(img, template_list, prev_loc=None):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #template = template[:,:,0]
+    #print(template.shape)
+    cv2.imshow("bbox", template)
+    w, h = template[0].shape[::-1]
+    method = eval('cv2.TM_SQDIFF')
+
+    # Apply template Matching
+    start_time = time.time()
+    #res = cv2.matchTemplate(img2,template,method)
+    #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    #print("--- %s seconds ---" % (time.time() - start_time))
+
+
+    #if min_val > 450000.0:
+    #    return
+
+
+    found = None
+    for template in template_list:
+        for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+            # resize the image according to the scale, and keep track
+            # of the ratio of the resizing
+            resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
+            resized = edge(resized)
+            r = gray.shape[1] / float(resized.shape[1])
+     
+            # if the resized image is smaller than the template, then break
+            # from the loop
+            if resized.shape[0] < h or resized.shape[1] < w:
+                break
+
+            result = cv2.matchTemplate(resized, template, method)
+            (min_val, max_val, min_loc, max_loc) = cv2.minMaxLoc(result)
+     
+            # check to see if the iteration should be visualized
+            if False:
+                # draw a bounding box around the detected region
+                clone = np.dstack([edged, edged, edged])
+                cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+                    (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+                cv2.imshow("Visualize", clone)
+                cv2.waitKey(0)
+     
+            # if we have found a new maximum correlation value, then update
+            # the bookkeeping variable
+            if found is None or min_val < found[0]:
+                found = (min_val, min_loc, r)
+
+
+    # # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    # if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+    #     top_left = min_loc
+    # else:
+    #     top_left = max_loc
+    # bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    # print(top_left, bottom_right)
+
+    # cv2.rectangle(img,top_left, bottom_right, 255, 2)
+    #return img
+
+    (v, maxLoc, r) = found
+    print(v)
+    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+    (endX, endY) = (int((maxLoc[0] + w) * r), int((maxLoc[1] + h) * r))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    #if prev_loc and abs(maxLoc[0] - prev_loc[0]) > 10 and v > 400000.0:
+    #    return
+
+    img[img[:,:,0] < 150] = 0
+
+    #img = img[img[:,:,0] > 150]
+ 
+    # draw a bounding box around the detected result and display the image
+    cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    return maxLoc
+
  
 if __name__ == '__main__':
  
