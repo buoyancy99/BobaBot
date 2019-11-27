dbf8e2fc8882f1a391509a1fd9fa842c481b66c4
Varun Arora <varunarora@Varuns-MacBook-Pro.local>
2019-11-27 13:31:38 -0800

    ding

---
+++ voice_utils/HearDing.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.io import wavfile # get the api
+from scipy.signal import stft
+from scipy.signal import fftconvolve
+import numpy as np
+
+
+fs, data = wavfile.read('Ding.wav') # load OG file
+a = data.T[0] 
+
+fs2, data2 = wavfile.read('Short.wav') # load the data
+a2 = data2.T[0]
+
+d = fftconvolve(a, a2)
+print(d.shape)
+for i in range(len(d)):
+	if d[i] > 0.85: #Tune this for the DING
+		print('Do something')
+		break
+plt.plot(d)
+plt.show()
---
+++ voice_utils/audio.py
@@ -0,0 +1,4 @@
+roslaunch audio_capture capture.launch #Launch don't close
+rostopic hz /audio #Launch to stream audio
+
+roslaunch audio_play play.launch #Plays incoming messages
