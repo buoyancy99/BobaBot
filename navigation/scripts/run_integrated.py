23e1dcb59438559f2fc190ef1a3c3ac7c9707fbb
Alexander Zhou <alexzhou00@berkeley.edu>
2019-12-02 18:52:19 -0800

    real integrated script

--- navigation/scripts/run_integrated.py
+++ navigation/scripts/run_integrated.py
@@ -8,60 +8,90 @@ from set_goal import Navigation
 
 if __name__ == '__main__':
 
-	launcher_lock = threading.Lock()
 	def elevator_done_cb(msg):
 		global launcher
-		global launcher_lock
-		with launcher_lock:
-			if launcher:
-				launcher.shutdown()
-				launcher = None
+		if launcher:
+			launcher.shutdown()
+			launcher = None
 
 	def launch(file):
 		global launcher
 		launcher = roslaunch.parent.ROSLaunchParent(uuid, launch_files[file])
 		launcher.start()
 
-	rospy.init_node('SDH_controller', anonymous=True)
-	elevator_sub = rospy.Subscriber('/elevator/done', Bool, elevator_done_cb)
-	launcher = None
-
-	SDH7_args = ['navigation', 'SDH7_enter_elevator.launch']
-	SDH7_launch_file = roslaunch.rlutil.resolve_launch_arguments(SDH7_args)
-
-	# SDH_7to2_args = ['navigation', 'SDH_7to2.launch']
-	# SDH_7to2_launch_file = roslaunch.rlutil.resolve_launch_arguments(SDH_7to2_args)
-
-	# SDH2_args =
-	# SDH2_launch_file = 
-
-	launch_files = {}
-	launch_files['SDH7'] = SDH7_launch_file
-	# launch_files['SDH_7to2'] = SDH_7to2_launch_file
-	# launch_files['SDH2'] = SDH2_launch_file
-
-	move = Navigation()
-	# raw_input("Tell me when to start")
-	# SDH7 start navigation
-	# move.set_init_pose(pos=[24.7214736938,6.34529781342,0],ori=[0,0,0.0795551282507,0.996830467817],cov=[0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787])
-	# result = move.movebase_client(pos=[50.009576416,4.13174282913,0],ori=[0,0,0.0123519308175,0.999923711993])
-
-	# Start SDH 7 Elevator script
-	# Uncomment below for unit testing 7th floor elevator.
-	move.set_init_pose(pos=[50.009576416,4.13174282913,0],ori=[0,0,0.0123519308175,0.999923711993],cov=[0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787])
-	uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-	roslaunch.configure_logging(uuid)
-	launch('SDH7')
-	print('Transferring control to position controller')
-	while not rospy.is_shutdown():
-		r = rospy.Rate(1) # 1hz
-		r.sleep()
-		with launcher_lock:
+	def wait_finish():
+		global launcher
+		while not rospy.is_shutdown():
+			r = rospy.Rate(1)
+			try:
+				r.sleep()
+			except:
+				pass
 			if launcher is None:
 				break
+
+	rospy.init_node('SDH_controller', anonymous=True)
+	elevator_sub = rospy.Subscriber('/elevator/done', Bool, elevator_done_cb)
+
+	launcher = None
+	uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+	roslaunch.configure_logging(uuid)
+
+	SDH7_enter_elevator_file = roslaunch.rlutil.resolve_launch_arguments(['navigation', 'SDH7_enter_elevator.launch'])
+	SDH2_exit_elevator_file = roslaunch.rlutil.resolve_launch_arguments(['navigation', 'SDH2_exit_elevator.launch'])
+	SDH2_enter_elevator_file = roslaunch.rlutil.resolve_launch_arguments(['navigation', 'SDH2_enter_elevator.launch'])
+	SDH7_exit_elevator_file = roslaunch.rlutil.resolve_launch_arguments(['navigation', 'SDH7_exit_elevator.launch'])
+
+	launch_files = {}
+	launch_files['SDH7_enter_elevator'] = SDH7_enter_elevator_file
+	launch_files['SDH2_exit_elevator'] = SDH2_exit_elevator_file
+	launch_files['SDH2_enter_elevator'] = SDH2_enter_elevator_file
+	launch_files['SDH7_exit_elevator'] = SDH7_exit_elevator_file
+
+	move = Navigation()
+	raw_input("Tell me when to start")
+	# SDH7 start navigation
+	move.set_init_pose(pos=[24.7214736938,6.34529781342,0],ori=[0,0,0.0795551282507,0.996830467817],cov=[0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787])
+	result = move.movebase_client(pos=[50.009576416,4.13174282913,0],ori=[0,0,0.0123519308175,0.999923711993])
+
+	# Uncomment below for unit testing 7th floor elevator.
+	move.set_init_pose(pos=[50.009576416,4.13174282913,0],ori=[0,0,0.0123519308175,0.999923711993],cov=[0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787])
+
+	# Start SDH 7 Elevator script
+	print('Starting SDH7 elevator script')
+	launch('SDH7_enter_elevator')
+	wait_finish()
 	print('Congrats on making it to the SDH 7 elevator!')
 
-	### EXIT ELEVATOR STUFF ###
+	# Start 7 to 2 to exit script
+	print('Starting inside elevator 7 to 2 script')
+	launch('SDH2_exit_elevator')
+	wait_finish()
+	print('Congrats on making it to SDH 2!')
 
+	# Go to cafe
+	result = move.movebase_client(pos=[-49.2158203125,-7.58403015137,0],ori=[0,0,0.708139986808,0.706072063661])
+
+	raw_input("Tell me when to return")
+	
+	# Go back to elevator
+	result = move.movebase_client(pos=[-25.4651012421,-3.54517769814,0],ori=[0,0,0.0123519308175,0.999923711993])
+        
+	# Start SDH 7 Elevator script
+	print('Starting SDH2 elevator script')
+	launch('SDH2_enter_elevator')
+	wait_finish()
+	print('Congrats on making it to the SDH 2 elevator!')
+
+	# Start 7 to 2 to exit script
+	print('Starting inside elevator 2 to 7 script')
+	launch('SDH7_exit_elevator')
+	wait_finish()
+	print('Congrats on making it to SDH 7!')
+
+	# Return to meeting room
+	result = move.movebase_client(pos=[24.7214736938,6.34529781342,0],ori=[0,0,0.0795551282507,0.996830467817])
+
+	print('Done!')
 
 	rospy.spin()
