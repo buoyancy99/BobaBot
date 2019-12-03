18723677f636012742b6e2460a541c4f3de236fa
Varun Arora <varunarora@airbears2-10-142-38-57.airbears2.1918.berkeley.edu>
2019-12-02 18:22:29 -0800

    Ding Floor 2 kind of

--- voice_utils/old_detection.py
+++ voice_utils/old_detection.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
-from scipy.signal import fftconvolve, convolve, stft
+from scipy.signal import fftconvolve, convolve, stft, butter
 import numpy as np
+from scipy import signal
 
 
 # fs, data = wavfile.read('Ding.wav') # load OG file
@@ -23,8 +24,8 @@ import numpy as np
 #import keyboard
 import pyaudio
 
-ding_left = np.load('ding_select_floor7_left_mic.npy')
-ding_right = np.load('ding_select_floor7_right_mic.npy')
+ding_left = np.load('ding_select_floor2_left_mic.npy')
+ding_right = np.load('ding_select_floor2_right_mic.npy')
 
 CHUNK = 4096 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
@@ -39,17 +40,25 @@ for _ in range(100):
 	for i in range(10): #to it a few times just to see
 	    data = np.frombuffer(stream.read(CHUNK),dtype=np.int16)
 	    data_buffer = np.concatenate([data_buffer, data])
-
+	
 	d_left = fftconvolve(ding_left, data_buffer)
 	d_right = fftconvolve(ding_right, data_buffer)
 	dlmax = d_left.max()
 	drmax = d_right.max()
 	print("left ding is:" +str(dlmax))
 	print("right ding is:" +str(drmax))
-	if dlmax > 20173224741.999992:
+	#FLOOR 7
+	# if dlmax > 20173224741.999992:
+	# 	print('Left DING')
+	# if drmax > 30888468567.000004:
+	# 	print('Right DING')
+	if dlmax > 10008361056.999992:
 		print('Left DING')
-	if drmax > 30888468567.000004:
+	if drmax > 2000511377.789566:
+
+
 		print('Right DING')
+	
 
 
 
